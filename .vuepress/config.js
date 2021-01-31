f5f441517d5d7e251f8fcc6d50023b02e0ab3ba0
Byeonghoon Lee <byeonghoonlee@Byeonghoonui-iMac.local>
2021-01-31 20:46:58 +0900

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  "title": "Elena's Workshop",
+  "description": "Actions speak louder than words",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home",
+      },
+      {
+        "text": "About me",
+        "link": "/about-me/index.md",
+        "icon": "reco-account"
+      },
+      {
+        "text": "Timeline",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      // {
+      //   "text": "Docs",
+      //   "icon": "reco-message",
+      //   "items": [
+      //     {
+      //       "text": "vuepress-reco",
+      //       "link": "/docs/theme-reco/"
+      //     }
+      //   ]
+      // },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "Linkedin",
+            "link": "http://linkedin.com/in/sumin-kim-632959165",
+            "icon": "reco-linkedin"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      // "/docs/theme-reco/": [
+      //   "",
+      //   "theme",
+      //   "plugin",
+      //   "api"
+      // ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    // "friendLink": [
+    //   {
+    //     "title": "午后南杂",
+    //     "desc": "Enjoy when you can, and endure when you must.",
+    //     "email": "1156743527@qq.com",
+    //     "link": "https://www.recoluan.com"
+    //   },
+    //   {
+    //     "title": "vuepress-theme-reco",
+    //     "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+    //     "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    //     "link": "https://vuepress-theme-reco.recoluan.com"
+    //   }
+    // ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Sumin Kim",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2021"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ about-me/index.md
@@ -0,0 +1,6 @@
+---
+title: About me
+date: 2020-01-30
+---
+
+Hi! My name is xxxx
